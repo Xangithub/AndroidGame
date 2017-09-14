7790b5bb54b1f07b4e3353c546c045b68627d754
aa <aa@bb.ru>
2017-09-15 01:08:41 +0300

    Initial commit

---
+++ desktop/src/ru/home/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.home.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.home.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
